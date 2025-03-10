f6022144356ea410ad4fd28afc5f39878ae82da2
sapientum8 <sapientum08@gmail.com>
2025-03-10 18:33:19 -0400

    req changes

--- handler.py
+++ handler.py
@@ -1,9 +1,14 @@
 import json
-
+import boto3
+import numpy
 
 def hello(event, context):
+
+    #print(numpy.max([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    v = boto3.__version__
+
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+        "message": "Go Serverless v4.0! Your function executed successfully! Boto3 version: " + v,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
